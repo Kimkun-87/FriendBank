1630b0731054a3ad2dd31d7e91e6037c2a7c5efe
kimminsu <iamkms1987@gmail.com>
2020-11-09 21:56:38 +0900

    no message

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.content1').click(function(){
+        $('.list1').slideToggle();
+    })
+
+    $('.content2').click(function(){
+        $('.list2').slideToggle();
+    })
+
+    $('.content3').click(function(){
+        $('.list3').slideToggle();
+    })
+
+    $('.content4').click(function(){
+        $('.list4').slideToggle();
+    })
+
+    $('.content5').click(function(){
+        $('.list5').slideToggle();
+    })
+
+    $('.content6').click(function(){
+        $('.list6').slideToggle();
+    })
+
+});
+
+
+
